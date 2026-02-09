84d8af374d03a79c53fbdaf04d4208b06a31bbd5
Theodore Puga <theopuga@gmail.com>
2026-02-09 16:16:18 -0500

    install update

--- install.sh
+++ install.sh
@@ -174,7 +174,7 @@ PY
   petals_venv="${BEAM_PETALS_VENV_DIR:-./beam-petals-venv}"
   echo "Installing Petals runtime in $petals_venv"
   python3 -m venv "$petals_venv"
-  "$petals_venv/bin/python" -m pip install --upgrade pip
+  "$petals_venv/bin/python" -m pip install --upgrade pip setuptools wheel
   if [[ -n "${BEAM_PETALS_TORCH_INDEX_URL:-}" ]]; then
     "$petals_venv/bin/python" -m pip install torch torchvision torchaudio --index-url "$BEAM_PETALS_TORCH_INDEX_URL"
   fi
